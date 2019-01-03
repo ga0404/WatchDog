4a938a048861d36515ffcc2202f8aa3c67f77060
ga0404 <ga0404@21cn.com>
2019-01-03 22:55:57 +0800

    Add sendpwd and listccard file to project

--- app/src/main/java/com/dodoiot/lockapp/controller/activity/LockDetailsActivity.java
+++ app/src/main/java/com/dodoiot/lockapp/controller/activity/LockDetailsActivity.java
@@ -442,11 +442,11 @@ public class LockDetailsActivity extends BaseActivity implements BaseNotifyListe
     }
 
     private void toSendPWd(){
-//        Intent intent = new Intent(LockDetailsActivity.this,SendPwdActivity.class);
-//        Bundle bundle = new Bundle();
-//        bundle.putSerializable("bean",bean);
-//        intent.putExtras(bundle);
-//        startActivity(intent);
+        Intent intent = new Intent(LockDetailsActivity.this,SendPwdActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("bean",bean);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 
     private void toManagePWd(){
